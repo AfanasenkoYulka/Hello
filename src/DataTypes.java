323b75623a5f25cfa0d193f4a3273bf1917f117a
Yulia <afanasenkoyulka@mail.ru>
2022-02-15 21:30:39 +0300

    Hello

---
+++ src/DataTypes.java
@@ -0,0 +1,15 @@
+public class DataTypes {
+    public static void main(String[] args) {
+        int n=5/3;
+        System.out.println("n="+n);
+        double m=5/3;
+;       System.out.println("m="+m);
+    m=Math.sin(Math.PI/6);
+    System.out.println("m="+m);
+
+    n=6/2;
+
+    long k=234234234l;
+    }
+
+}
